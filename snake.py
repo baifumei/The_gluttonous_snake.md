a7e6bec5b48e9f730e6e800af597ea969404de2a
baifumei <k_katerina_k@icloud.com>
2022-01-23 23:30:13 +0300

    Creat canvas

--- snake.py
+++ snake.py
@@ -1,3 +1,18 @@
 import pygame
 import random
 from pygame.locals import *
+
+pygame.init()
+
+
+dis = pygame.display.set_mode((500, 500))
+pygame.display.update()
+pygame.display.set_caption('Snake game by Edureka')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+pygame.quit()
+quit()
