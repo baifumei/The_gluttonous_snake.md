7a44eed37b4508d1bb6b442683ee2900395ba248
baifumei <k_katerina_k@icloud.com>
2022-01-24 00:32:43 +0300

    Now snake can move

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ pygame.init()
 
 windw_width, windw_height = 600, 500
 windw = pygame.display.set_mode((windw_width, windw_height))
+background = pygame.image.load('/Users/ekaterinak/Downloads/696.jpg')
 pygame.display.set_caption("Прожорливая змейка")
 
 # параметры
@@ -20,13 +21,43 @@ y_food = 50
 speed = 5
 snake_color = (0, 255, 90)
 
+# действия
 game_over = False
 while not game_over:
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
-# creat a snake)))
+    move = pygame.key.get_pressed()
+    if move[pygame.K_LEFT]:
+        delta_x = -1
+        delta_y = 0
+    elif move[pygame.K_RIGHT]:
+        delta_x = 1
+        delta_y = 0
+    elif move[pygame.K_UP]:
+        delta_x = 0
+        delta_y = -1
+    elif move[pygame.K_DOWN]:
+        delta_x = 0
+        delta_y = 1
+
+    x += delta_x * speed
+    y += delta_y * speed
+
+    if x > windw_width:
+        x = 0
+    elif x < 0:
+        x = windw_width
+    elif y > windw_height:
+        y = 0
+    elif y < 0:
+        y = windw_height
+
+    windw.fill((0, 0, 0))
+    windw.blit(background, (0, 0))
+
     pygame.draw.rect(windw, snake_color, (x, y, height, width))
     pygame.display.update()
 
